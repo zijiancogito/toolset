a5e250b521019e4ab8ceaf62a1be8d80c6bb6fbb
zijiancogito <345981440@qq.com>
2020-01-13 01:22:47 +0800

    fix

--- generator/extract_lines.py
+++ generator/extract_lines.py
@@ -8,10 +8,16 @@ from utils.listdir import list_all_files
 
 
 def read_file(filename):
+  print(filename)
   with open(filename, 'r') as f:
     stream = f.read()
     tmp = yaml.load(stream)
-    pdb.set_trace()
+    #pdb.set_trace()
+    basename, ext = os.path.split(os.path.basename(filename))
+    cfile = os.path.join(os.path.dirname(filename), "list/%s.c", basename)
+    sfile = os.path.join(os.path.dirname(filename), "list/%s.s", basename)
+    cf = open(cfile, 'w')
+    sf = open(sfile, 'w')
     for line in tmp:
       c_block = line[0]
       if c_block == '{' or c_block == '}':
@@ -21,12 +27,18 @@ def read_file(filename):
       for addr in asm_block:
         if c_block != '{':
           asm_list.append(asm_block[addr])
-      pdb.set_trace()
+      #pdb.set_trace()
       asm = AsmParser(';'.join(asm_list))
       new_asm = asm.new_asm
       table = asm.imm_map
       c = CParser(c_block)
       new_c = c.sub_table(table)
+      cf.write(new_c)
+      cf.write('\n')
+      sf.write(new_asm)
+      sf.write('\n')
+    cf.close()
+    sf.close()
 
 
 if __name__ == '__main__':
